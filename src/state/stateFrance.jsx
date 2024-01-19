6493de4feade40c7e02318ce79210370817436a5
Igor <igorivankov56@gmail.com>
2024-01-19 16:04:32 +0500

    a

--- src/state/stateFrance.jsx
+++ src/state/stateFrance.jsx
@@ -1,5 +1,5 @@
 let stateFrance = [
-    { img: './img.amur.jpg', creator: 'Марсель Руссо', name: 'Охота Амура', size: 'Холст, масло (50х80)', price: '14 500 руб' },
+    { img: './img/amur.jpg', creator: 'Марсель Руссо', name: 'Охота Амура', size: 'Холст, масло (50х80)', price: '14 500 руб' },
     { img: './img/lady.jpg', creator: 'Анри Селин', name: 'Дама с собачкой', size: 'Акрил, бумага (50х80) ', price: '16 500 руб' },
     { img: './img/procedure.jpg', creator: 'Франсуа Дюпон', name: 'Процедура', size: 'Цветная литография (40х60) ', price: '20 000 руб' },
     { img: './img/rose.jpg', creator: 'Луи Детуш', name: 'Роза', size: 'Холст, масло (50х80)', price: '12 000 руб' },
