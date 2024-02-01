d2bdd64458212dd352726539904563fc64595a25
Igor <igorivankov56@gmail.com>
2024-02-02 00:56:09 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import stateFrance from './state/stateFrance';
 import stateGermany from './state/sateGermany';
 import stateEngland from './state/stateEngland';
 
+const basename = document.querySelector('base')?.getAttribute('href') ?? '/' 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App stateEngland={stateEngland} stateGermany={stateGermany} stateFrance={stateFrance} />
     </BrowserRouter>
   </React.StrictMode>
